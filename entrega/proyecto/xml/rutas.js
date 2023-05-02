109c115803556c5fe3b34e1c197c91f2a406b6aa
UO265181 <UO265181@uniovi.es>
2023-05-02 16:50:59 +0200

    error de nombres

--- entrega/proyecto/xml/rutas.js
+++ entrega/proyecto/xml/rutas.js
@@ -15,22 +15,89 @@ class ArchivoXML {
 
                 $('ruta', datos).each(function () {
                     var ruta = $(this).attr('nombre');
-                    var tipo = $(this).attr('nombre');
-                    var medio_transporte = $(this).attr('nombre');
-                    var recomendacion = $(this).attr('nombre');
-                    
+                    var tipo = $(this).attr('tipo');
+                    var medio_transporte = $(this).attr('medio_transporte');
+                    var recomendacion = $(this).attr('recomendacion');
+
                     var duracion = $(this).find('duracion').text();
                     var agencia = $(this).find('agencia').text();
                     var descripcion = $(this).find('descripcion').text();
                     var personas = $(this).find('personas').text();
-                
+
 
                     var section = $('<section>').appendTo('body');
-                    $('<h2>').text(ruta).appendTo(div);
-                    $('<p>').text('tipo: ' + tipo).appendTo(div);
-                    $('<p>').text('medio_transporte: ' + medio_transporte).appendTo(div);
-                    $('<p>').text('recomendacion: ' + recomendacion).appendTo(div);
+                    $('<h2>').text(ruta).appendTo(section);
+                    $('<p>').text('tipo: ' + tipo).appendTo(section);
+                    $('<p>').text('medio_transporte: ' + medio_transporte).appendTo(section);
+                    $('<p>').text('recomendacion: ' + recomendacion).appendTo(section);
                 });
+
+
+                /*
+                $("ruta", xml).each(function () {
+    // Obtener los atributos de la ruta
+    var tipo = $(this).attr("tipo");
+    var recomendacion = $(this).attr("recomendacion");
+
+    // Crear la sección de la ruta
+    var seccionRuta = $("<section>").addClass("ruta");
+
+    // Crear el encabezado de la sección
+    var encabezadoRuta = $("<h2>").text(tipo + " (Recomendación: " + recomendacion + ")");
+    seccionRuta.append(encabezadoRuta);
+
+    // Recorrer los hitos de la ruta
+    $("hito", this).each(function () {
+        // Obtener los datos del hito
+        var nombre = $("nombre", this).text();
+        var descripcion = $("descripcion", this).text();
+        var longitud = $("longitud", this).text();
+        var latitud = $("latitud", this).text();
+        var altitud = $("altitud", this).text();
+        var distancia = $("distancia", this).text();
+        var unidades = $("distancia", this).attr("unidades");
+        var fotos = $("galeria_fotos foto", this).map(function () {
+            return $(this).text();
+        }).get();
+        var videos = $("galeria_videos video", this).map(function () {
+            return $(this).text();
+        }).get();
+
+        // Crear la sección del hito
+        var seccionHito = $("<section>").addClass("hito");
+
+        // Crear el encabezado del hito
+        var encabezadoHito = $("<h3>").text(nombre);
+        seccionHito.append(encabezadoHito);
+
+        // Crear el párrafo de la descripción
+        var parrafoDescripcion = $("<p>").text(descripcion);
+        seccionHito.append(parrafoDescripcion);
+
+        // Crear la lista de coordenadas
+        var listaCoordenadas = $("<ul>").addClass("coordenadas");
+        var itemCoordenadasLongitud = $("<li>").text("Longitud: " + longitud);
+        var itemCoordenadasLatitud = $("<li>").text("Latitud: " + latitud);
+        var itemCoordenadasAltitud = $("<li>").text("Altitud: " + altitud);
+        listaCoordenadas.append(itemCoordenadasLongitud, itemCoordenadasLatitud, itemCoordenadasAltitud);
+        seccionHito.append(listaCoordenadas);
+
+        // Crear el párrafo de la distancia
+        var parrafoDistancia = $("<p>").text("Distancia: " + distancia + " " + unidades);
+        seccionHito.append(parrafoDistancia);
+
+        // Crear la lista de fotos
+        var listaFotos = $("<ul>").addClass("galeria-fotos");
+        fotos.forEach(function (foto) {
+            var itemFoto = $("<li>").html($("<img>").attr("src", foto));
+            listaFotos.append(itemFoto);
+        });
+        seccionHito.append(listaFotos);
+
+        // Crear la lista de videos
+        var listaVideos = $("<ul>").addClass("galeria-videos");
+        videos.forEach(function (video) {
+            var itemVideo = $("<li>").html($("<iframe>").attr("src",*/
 
 
             },
