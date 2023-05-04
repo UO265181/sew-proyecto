7d8cb4962e580c7a1f764687549412149e8dc560
UO265181 <UO265181@uniovi.es>
2023-05-04 19:10:46 +0200

    Se muestran las coordenadas al final de cada ruta KML generado no funcional

--- entrega/proyecto/xml/rutas.js
+++ entrega/proyecto/xml/rutas.js
@@ -5,6 +5,24 @@ class ArchivoXML {
         this.nombre = nombre;
         this.correcto = "¡Todo correcto! archivo XML cargado"
     }
+
+
+
+
+
+
+
+
+    generarKML(coordenadas, section) {
+
+
+
+    }
+
+
+
+
+
     cargarDatos() {
         $.ajax({
             dataType: "xml",
@@ -14,6 +32,9 @@ class ArchivoXML {
 
 
                 $('ruta', datos).each(function () {
+
+                    var coordenadasRuta = [];
+
                     var ruta = $(this).attr('nombre');
                     var tipo = $(this).attr('tipo');
                     var medio_transporte = $(this).attr('medio_transporte');
@@ -29,11 +50,12 @@ class ArchivoXML {
                     var inicio_hora = $(inicio).find('hora').text();
                     var inicio_lugar = $(inicio).find('lugar').text();
                     var inicio_direccion = $(inicio).find('direccion').text();
-                    var coordenadas = $(this).find('coordenadas');
+                    var coordenadas = $(this).find('coordenadas')[0];
                     var longitud = $(coordenadas).find('longitud').text();
                     var latitud = $(coordenadas).find('latitud').text();
                     var altitud = $(coordenadas).find('altitud').text();
 
+                    coordenadasRuta.push(coordenadas);
 
                     var section = $('<section>').appendTo('main');
                     $('<h2>').text(ruta).appendTo(section);
@@ -46,14 +68,14 @@ class ArchivoXML {
                     $('<li>').text('Recomendación: ' + recomendacion).appendTo(datosDeRuta);
                     $('<li>').text('Agencia: ' + agencia).appendTo(datosDeRuta);
                     $('<li>').text('Personas: ' + personas).appendTo(datosDeRuta);
-                    $('<li>').text('Duración ('+duracion_unidades+'): ' + duracion).appendTo(datosDeRuta);
+                    $('<li>').text('Duración (' + duracion_unidades + '): ' + duracion).appendTo(datosDeRuta);
                     datosDeRuta.appendTo(section);
 
                     $('<h3>').text("Inicio").appendTo(section);
                     var datosDeInicio = $('<ul>');
-                    if(inicio_fecha.length>0)
+                    if (inicio_fecha.length > 0)
                         $('<li>').text('Fecha: ' + inicio_fecha).appendTo(datosDeInicio);
-                    if(inicio_hora.length>0)
+                    if (inicio_hora.length > 0)
                         $('<li>').text('Hora: ' + inicio_hora).appendTo(datosDeInicio);
                     $('<li>').text('Lugar: ' + inicio_lugar).appendTo(datosDeInicio);
                     $('<li>').text('Dirección: ' + inicio_direccion).appendTo(datosDeInicio);
@@ -65,6 +87,7 @@ class ArchivoXML {
                     coordenadasItem.appendTo(datosDeInicio);
                     datosDeInicio.appendTo(section);
 
+                    
 
                     $("hito", this).each(function () {
                         var hito = $(this).attr('nombre');
@@ -76,15 +99,16 @@ class ArchivoXML {
                         var latitud = $(coordenadas).find('latitud').text();
                         var altitud = $(coordenadas).find('altitud').text();
 
+                        coordenadasRuta.push(coordenadas);
 
-                        
+
                         var sectionHito = $('<section>').appendTo(section);
                         $('<h3>').text(hito).appendTo(sectionHito);
 
                         $('<p>').text(descripcion).appendTo(sectionHito);
 
                         var datosDeHito = $('<ul>');
-                        $('<li>').text('Distancia ('+distancia_unidades+'): ' + distancia).appendTo(datosDeHito);
+                        $('<li>').text('Distancia (' + distancia_unidades + '): ' + distancia).appendTo(datosDeHito);
                         var coordenadasItem = $('<li>').text("Coordenadas: ")
                         var coordenadasLista = $('<ul>').appendTo(coordenadasItem);
                         $('<li>').text('Longitud: ' + longitud).appendTo(coordenadasLista);
@@ -117,41 +141,70 @@ class ArchivoXML {
                                 "controls": true,
                                 "preload": "auto",
                             });
-                            $("<source>").attr("src",src).attr("type",type).appendTo(video);
+                            $("<source>").attr("src", src).attr("type", type).appendTo(video);
                             video.appendTo(seccionMultimedia);
                         });
 
 
                     });
 
+
+                    //KML-----------------------------
+
+                    var kmlDoc = document.implementation.createDocument('', 'kml', null);
+                    var kmlElement = kmlDoc.documentElement;
+
+                    var documentElement = kmlDoc.createElement('Document');
+                    kmlElement.appendChild(documentElement);
+
+
+                    for (var i = 0; i < coordenadasRuta.length; i++) {
+                        var latitud = $(coordenadasRuta[i]).find('latitud').text();
+                        var longitud = $(coordenadasRuta[i]).find('longitud').text();
+
+                        var placemarkElement = kmlDoc.createElement('Placemark');
+
+                        var pointElement = kmlDoc.createElement('Point');
+                        placemarkElement.appendChild(pointElement);
+
+                        var coordinatesElement = kmlDoc.createElement('coordinates');
+                        coordinatesElement.textContent = longitud + ',' + latitud;
+                        pointElement.appendChild(coordinatesElement);
+
+                        documentElement.appendChild(placemarkElement);
+                    }
+
+                    var serializer = new XMLSerializer();
+                    var kmlString = serializer.serializeToString(kmlDoc);
+                    section.append(kmlString);
+
+
+
+
+
+                    var kmlUrl = URL.createObjectURL(new Blob([kmlDoc], {type: 'application/vnd.google-earth.kml+xml'}));
+
+                    // Crear la capa de KML
+                    var kmlLayer = new L.KML(kmlUrl, {async: true});
+        
+                    // Crear el mapa
+                    var map = L.map('map').setView([43.357, -5.853], 10);
+        
+                    // Añadir la capa de KML al mapa
+                    kmlLayer.appendTo(section);
+
                 });
-
-
-                /*
-
-        // Crear la lista de fotos
-        var listaFotos = $("<ul>").addClass("galeria-fotos");
-        fotos.forEach(function (foto) {
-            var itemFoto = $("<li>").html($("<img>").attr("src", foto));
-            listaFotos.append(itemFoto);
-        });
-        seccionHito.append(listaFotos);
-
-        // Crear la lista de videos
-        var listaVideos = $("<ul>").addClass("galeria-videos");
-        videos.forEach(function (video) {
-            var itemVideo = $("<li>").html($("<iframe>").attr("src",*/
-
-
             },
             error: function () {
-                $("h3").html("¡Tenemos problemas! No se pudo cargar el archivo XML");
-                $("h4").remove();
-                $("h5").remove();
-                $("p").remove();
+                $("h1").html("ERROR: No se pudo cargar el archivo XML");
             }
         });
     }
+
+
+
+
+
     crearElemento(tipoElemento, texto, insertarAntesDe) {
         // Crea un nuevo elemento modificando el árbol DOM
         // El elemnto creado es de 'tipoElemento' con un 'texto' 
