08c7ed320fa472a3ce69e414de29b4d24c936b38
UO265181 <UO265181@uniovi.es>
2023-05-06 13:36:36 +0200

    mejora de código de carrusel.js

--- entrega/proyecto/js/carrusel.js
+++ entrega/proyecto/js/carrusel.js
@@ -4,9 +4,10 @@ class Carrusel {
     constructor(images) {
         this.images = images;
         this.currentIndex = 0;
+        this.contenedor = null;
     }
 
-    render(contenedor) {
+    render() {
         var img = document.createElement('img');
         img.src = this.images[this.currentIndex].src;
         img.alt = this.images[this.currentIndex].alt;
@@ -14,13 +15,12 @@ class Carrusel {
         var figCaption = document.createElement('figCaption');
         figCaption.textContent = this.images[this.currentIndex].figCaption;
 
-        contenedor.innerHTML = '';
-        contenedor.appendChild(img);
-        contenedor.appendChild(figCaption);
+        this.contenedor.innerHTML = '';
+        this.contenedor.appendChild(img);
+        this.contenedor.appendChild(figCaption);
     }
 
-    start(interval, contenedor) {
-        this.contenedor = contenedor;
+    start(interval) {
         this.render(this.contenedor);
         this.intervalId = setInterval(() => this.next(), interval);
     }
@@ -34,30 +34,44 @@ class Carrusel {
         this.currentIndex = (this.currentIndex + this.images.length - 1) % this.images.length;
         this.render(this.contenedor);
     }
+
+
+    createContainer() {
+        const section = document.createElement('section');
+
+        const h2 = document.createElement('h2')
+        h2.textContent = 'Carrusel de imágenes';
+
+        const figure = document.createElement('figure');
+        const buttonPrev = document.createElement('button');
+        buttonPrev.textContent = 'Anterior';
+        buttonPrev.addEventListener('click', () => carrusel.prev());
+        const buttonNext = document.createElement('button');
+        buttonNext.textContent = 'Siguiente';
+        buttonNext.addEventListener('click', () => carrusel.next());
+
+        section.appendChild(h2);
+        section.appendChild(figure);
+        section.appendChild(buttonPrev);
+        section.appendChild(buttonNext);
+
+        document.getElementsByTagName('main')[0].appendChild(section);
+
+        this.contenedor = figure;
+    }
+
 }
 
 const images = [
-    {src:'multimedia/carrusel1.jpg', alt:'Foto de la placa de Cabranes', figCaption: 'Placa de Cabranes'},
-    {src:'multimedia/carrusel2.jpg', alt:'Foto de la cima del monte Incós', figCaption: 'Monte Incós'},
-    {src:'multimedia/carrusel3.jpg', alt:'Foto de la Iglesia de San Martín el Real en Torazo', figCaption: 'Iglesia de San Martín el Real'},
-    {src:'multimedia/carrusel4.jpg', alt:'Foto de los vecinos reunidos en el festival del arroz', figCaption: 'Festival de Arroz'},
-    {src:'multimedia/carrusel5.jpg', alt:'Foto del antiguo castillete minero', figCaption: 'Castillete minero'}];
+    { src: 'multimedia/carrusel1.jpg', alt: 'Foto de la placa de Cabranes', figCaption: 'Placa de Cabranes' },
+    { src: 'multimedia/carrusel2.jpg', alt: 'Foto de la cima del monte Incós', figCaption: 'Monte Incós' },
+    { src: 'multimedia/carrusel3.jpg', alt: 'Foto de la Iglesia de San Martín el Real en Torazo', figCaption: 'Iglesia de San Martín el Real' },
+    { src: 'multimedia/carrusel4.jpg', alt: 'Foto de los vecinos reunidos en el festival del arroz', figCaption: 'Festival de Arroz' },
+    { src: 'multimedia/carrusel5.jpg', alt: 'Foto del antiguo castillete minero', figCaption: 'Castillete minero' }];
 
-document.write('<section>');
-document.write('<h2> Carrusel de imágenes de Cabranes </h2>');
-document.write('<figure id="carrusel"></figure>');
-document.write('<button id="prev">Anterior</button>');
-document.write('<button id="next">Siguiente</button>');
-document.write('</section>');
-
-//TODO: se puede usar esto??
-const contenedor = document.getElementById('carrusel');
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
 
 //TODO: la primera vez que se renderiza una imagen se recarga la página entera
 const carrusel = new Carrusel(images);
-carrusel.start(3000, contenedor);
+carrusel.createContainer();
+carrusel.start(3000);
 
-prevButton.addEventListener('click', () => carrusel.prev());
-nextButton.addEventListener('click', () => carrusel.next());
